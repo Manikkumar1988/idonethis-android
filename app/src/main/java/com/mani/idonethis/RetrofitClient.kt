25c87762237c7f6d0308ecdd70bb79b49ca19b03
Manikkumar K <maniksrr@gmail.com>
2019-11-01 20:27:48 +0530

    Retrofit client

---
+++ app/src/main/java/com/mani/idonethis/RetrofitClient.kt
@@ -0,0 +1,25 @@
+package com.mani.idonethis
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+//        .readTimeout(60, TimeUnit.SECONDS)
+//        .connectTimeout(60, TimeUnit.SECONDS)
+        .build()
+
+    fun provideRetrofit(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    fun <T> provideApiService(retrofit: Retrofit, apiServiceClass: Class<T>): T {
+        return retrofit.create(apiServiceClass)
+    }
+}
