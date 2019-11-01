7d68b5cd64d6f18fb64b3a768843cfc100507ebf
Manikkumar K <maniksrr@gmail.com>
2019-11-02 01:32:58 +0530

    completed home fragment

--- app/src/main/java/com/mani/idonethis/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mani/idonethis/ui/home/HomeFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mani.idonethis.R
+import com.mani.idonethis.databinding.FragmentHomeBinding
+import com.mani.idonethis.databinding.FragmentLoginBinding
+import com.mani.idonethis.ui.home.model.DoneItem
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
@@ -22,11 +27,18 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater,
+            R.layout.fragment_home, container, false
+        )
 
+        binding.addButton.setOnClickListener{
+            homeViewModel.addTodo(DoneItem(binding.todoEditText.text.toString() ,0))
+            Toast.makeText(context!!,"Added",Toast.LENGTH_SHORT).show()
+        }
 
         (activity as? AppCompatActivity)?.supportActionBar?.show()
 
-        return root
+        return binding.root
     }
 }
