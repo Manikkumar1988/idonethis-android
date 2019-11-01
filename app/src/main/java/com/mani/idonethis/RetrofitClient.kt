f05a5ac87c6ed7aa4fc783cc3cdc32f8c03d7e04
Manikkumar K <maniksrr@gmail.com>
2019-11-01 21:46:56 +0530

    Retrofit increased timeout incase Heroku is down for long time

--- app/src/main/java/com/mani/idonethis/RetrofitClient.kt
+++ app/src/main/java/com/mani/idonethis/RetrofitClient.kt
@@ -3,12 +3,13 @@ package com.mani.idonethis
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
     private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
-//        .readTimeout(60, TimeUnit.SECONDS)
-//        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .connectTimeout(60, TimeUnit.SECONDS)
         .build()
 
     fun provideRetrofit(baseUrl: String): Retrofit {
